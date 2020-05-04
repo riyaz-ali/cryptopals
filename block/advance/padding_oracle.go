30e76c4c745f8ccf4aba22e88fd1d8faf69e87ae
Riyaz Ali <me@riyazali.net>
2020-05-04 14:14:32 +0530

    docs: update solution to make it more readable

--- block/advance/padding_oracle.go
+++ block/advance/padding_oracle.go
@@ -48,20 +48,25 @@ func PaddingOracleAttack(enc func() []byte, dec func([]byte) error) ([]byte, err
 	// manipulate values in ciphertext of block [:bs] to cause changes in plaintext of block[bs : 2*bs]
 	for len(out) > bs {
 		var result, intermediate [bs]byte
-		var block = out[:2*bs]
+		var c0, c1 = make([]byte, bs), make([]byte, bs)
+		copy(c0, out[:bs])
+		copy(c1, out[bs:2*bs])
 
 		for i := 0; i < bs; i++ {
+			// the current byte position that we're decrypting
+			var pos = bs - i - 1
+			var value = byte(i + 1) // the expected value that would generate a correct padding
+
+			// forge fake values using existing knowledge of intermediary values
+			// to generate expected padding in i-1 blocks
 			for k := 0; k < i; k++ {
-				block[bs-k-1] = byte(i+1) ^ intermediate[bs-k-1]
+				c0[bs-k-1] = value ^ intermediate[bs-k-1]
 			}
 
-			// save the original byte to recover the real plaintext later
-			var org = block[bs-i-1]
-
 			// for all possible byte combination
-			for j := 1; j < 256; j++ {
-				block[bs-i-1] = byte(j)
-				if err := dec(block); err == nil {
+			for j := 0; j < 256; j++ {
+				c0[pos] = byte(j)
+				if err := dec(append(c0, c1...)); err == nil {
 					// work around for false positive cases
 					// ------------------------
 					// this is a _really_ interesting edge cases
@@ -76,10 +81,10 @@ func PaddingOracleAttack(enc func() []byte, dec func([]byte) error) ([]byte, err
 					// was to set the byte to 0x01 and "recover" 13 as a result
 					// a better description to this could be found at [https://eklitzke.org/the-cbc-padding-oracle-problem]
 					if i == 0 {
-						var org1 = block[bs-i-2]
-						block[bs-i-2] = 0xff
-						err = dec(block)
-						block[bs-i-2] = org1
+						var a = c0[pos-1]
+						c0[pos-1] = 0xff
+						err = dec(append(c0, c1...))
+						c0[pos-1] = a
 						if err != nil {
 							continue
 						}
@@ -88,10 +93,10 @@ func PaddingOracleAttack(enc func() []byte, dec func([]byte) error) ([]byte, err
 					// the intermediate byte (one that's decrypted but not XOR'd)
 					// is given by equation, Dk(c1) = p1 ^ c0
 					// for byte position n, Dk(c1)[n] = p1[n] ^ c0[n]
-					// in our case, we know p1[n] will be equal to (i + 1) or current index (see PKCS7)
+					// in our case, we know p1[n] will be equal to (value + 1) or current index (see PKCS7)
 					// so, to get the intermediate we do:
-					intermediate[bs-i-1] = byte((i + 1) ^ j)
-					result[bs-i-1] = intermediate[bs-i-1] ^ org
+					intermediate[pos] = value ^ byte(j)
+					result[pos] = intermediate[pos] ^ out[pos] // get real plaintext by XORing with original C0 byte
 					break
 				}
 			}
